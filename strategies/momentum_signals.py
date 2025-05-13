ad2fead9e53d6d9f945b2798a2a4076934186d46
Pal Akhilesh Singh <134354463+go-akhi@users.noreply.github.com>
2025-05-13 14:01:45 +0900

    Update momentum_signals.py
    working on MACD strategies, commiting unfinished work because all I had access to was the github website

--- strategies/momentum_signals.py
+++ strategies/momentum_signals.py
@@ -8,7 +8,38 @@ stock_data.columns = stock_data.columns.map(lambda x : x[0] )
 
 momentum_bucket = ["RSI", "MACD histogram heights", "ROC", "Stochastic Oscillator", "MFI"]
 
-def calculate_rsi_strength(output_df, lower_limit = 30, upper_limit = 80 , stock_data):
+def is_column(df,col):
+    if col not in df.columns():
+        raise ValueError(f"The DataFrame does not contain the required column. The DataFrame does not have the {col} column.")
+
+def rsi_signal(output_df, buy_limit = 30, sell_limit = 70 , stock_data):
     """
     Generates signal and strength based on the RSI value and the threshold set by the user
+
+    Parameters:
+        output_df(pd.DataFrame): The dataframe to which the signals and strengths will be 
+            written to
+        buy_limit(int): The oversold limit below which the function generates a buy signal
+        sell_imit(int): The overbought limit over which the function makes a sell signal
+        stock_data(pd.DataFrame): DataFrame with a RSI column
+
+    Returns:
+        output_df(pd.DataFrame): A DataFrame with appended signal and strength columns
     """
+    # Ensure the data exists
+    is_column(stock_data, "RSI")
+
+    #Compare the RSI values and generate signals and calculate strengths
+    for rsi in stock_data["RSI"]:
+        if rsi < buy_limit:
+            output_df["RSI signal"] = 1
+            output_df["RSI signal strength"] = (buy_limit - rsi) * 100 / buy_limit
+        elif rsi > sell_limit:
+            output_df["RSI signal"] = -1
+            output_df["RSI signal strength"] = (rsi - sell_limit) * 100 / (100 - sell_limit)
+        else:
+            output_df["RSI signal"] = 0
+            
+    return(output_df)
+
+def macd_signal(output_df, stock_data):
